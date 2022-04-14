046a746e44a73b6f6b0885771278cbd4c4dfde5b
Marion <36108987+codeMarion@users.noreply.github.com>
2022-04-14 19:51:12 +0100

    created responsive dialog

--- src/pages/Landing/detailOne.jsx
+++ src/pages/Landing/detailOne.jsx
@@ -1,8 +1,15 @@
 import { ReactComponent as SelectSvg } from "../../images/svg/select.svg";
-import React from "react";
-import Popup from "reactjs-popup";
+import React, {useState} from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Box from '@mui/material/Box';
 
 function DetailOne() {
+  const [open, setOpen] = useState(false)
   return (
     <>
       <div id="details" className="basic-2">
@@ -13,42 +20,38 @@ function DetailOne() {
             </div>
             <div className="col-lg-6">
               <div className="text-container">
-                <h2 style={{fontFamily: "Poppins"}}>Browse & Select</h2>
-                <p style={{fontFamily: "Poppins"}}>
+                <h2 style={{ fontFamily: "Poppins" }}>Browse & Select</h2>
+                <p style={{ fontFamily: "Poppins" }}>
                   Browse our endless supply of deals for UK products with our
                   tailored deal of the day page that will highlight our best
                   sales <br />
                   After finding the product you want, just sign up to our group
                   promotion sale
                 </p>
-                <Popup
-                  trigger={
-                    <button className="btn-solid-reg" style={{fontFamily: "Poppins"}}> Show More</button>
-                  }
-                  modal
-                >
-                  {(close) => (
-                    <div className="lightbox-basic">
-                      <div className="row">
-                        <button
-                          title="Close (Esc)"
-                          type="button"
-                          className="mfp-close x-button"
-                          onClick={close}
-                        >
-                          ×
-                        </button>
-                        <div className="col-lg-4">
-                          <img
-                            className="img-fluid"
-                            style={{height:"85%"}}
-                            src="images/home.png"
-                            alt="alternative"
-                          />
-                        </div>
-                        <div className="col-lg-4">
-                          <h3>Browse and Select</h3>
-                          <hr />
+                <button onClick={() => setOpen(true)} className="btn-solid-reg" style={{fontFamily: "Poppins"}}> Show More</button>
+                <Dialog maxWidth={'lg'} open={open} >
+                  <DialogTitle sx={{ m: 0, p: 2, backgroundColor: '#2f2c3d' }}>
+                    <IconButton
+                      onClick={() => setOpen(false)}
+                      sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                      }}
+                    >
+                      <CloseIcon />
+                    </IconButton>
+                  </DialogTitle>
+                  <DialogContent style={{ backgroundColor: '#2f2c3d', width: '100%' }}>
+                    <Grid container spacing={1}>
+                      <Grid item xs={12} sm={6} md={3} >
+                        <img src={"images/home.png"} width="100%" />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6} style={{ justifyContent: 'center' }}>
+                        <Box>
+                          <h4>Browse and Select</h4>
+                          <Box style={{height: '3px', width: '20%', backgroundColor: '#ff9200', marginBottom: '4%'}}></Box>
                           <p>
                             Whether you are looking to buy electronic devices
                             for house cleaning products you can find daily new
@@ -64,8 +67,7 @@ function DetailOne() {
                             our app after signing up for our group promotion
                             sale
                           </p>
-
-                          <h4>User Feedback</h4>
+                          <h5>User Feedback</h5>
                           <p>
                             You should definitely give it a try to get products
                             with best discounts
@@ -73,51 +75,48 @@ function DetailOne() {
                           <table>
                             <tr>
                               <td className="icon-cell">
-                                <i className="fas fa-mobile-alt"></i>
+                                <i style={{ color: '#ff9200' }} className="fas fa-mobile-alt"></i>
                               </td>
-                              <td>Interactive UI</td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>Interactive UI</td>
                             </tr>
                             <tr>
                               <td className="icon-cell">
-                                <i className="fas fa-tag"></i>
+                                <i style={{ color: '#ff9200' }} className="fas fa-tag"></i>
                               </td>
-                              <td>Huge Discounts on Different Prodcuts</td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>Huge Discounts on Different Prodcuts</td>
                             </tr>
                             <tr>
                               <td className="icon-cell">
-                                <i className="far fa-calendar-alt"></i>
+                                <i style={{ color: '#ff9200' }} className="far fa-calendar-alt"></i>
                               </td>
-                              <td>New and Best Deals Highlighted Everyday</td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>New and Best Deals Highlighted Everyday</td>
+                            </tr>
+                            <tr>
+                              <td className="icon-cell p">
+                                <i style={{ color: '#ff9200' }} className="fas fa-clipboard-check"></i>
+                              </td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>Keep Track of your selected products</td>
                             </tr>
                             <tr>
                               <td className="icon-cell">
-                                <i className="fas fa-clipboard-check"></i>
+                                <i style={{ color: '#ff9200' }} className="fas fa-id-card"></i>
                               </td>
-                              <td>Keep Track of your selected products</td>
-                            </tr>
-                            <tr>
-                              <td className="icon-cell">
-                                <i className="fas fa-id-card"></i>
-                              </td>
-                              <td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>
                                 Checkout Your Favourite Influencers Page For
                                 More Deals
                               </td>
                             </tr>
                           </table>
-                        </div>
-                        <div className="col-lg-4">
-                          <img
-                            className="img-fluid"
-                            style={{height:"85%"}}
-                            src="images/product.png"
-                            alt="alternative"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </Popup>
+                        </Box>
+                      </Grid>
+                      <Grid item xs={12} sm={3} md={3}>
+                        <Box sx={{ display: { xs:'none', sm: 'none', md: 'block'} }}>
+                          <img src={"images/product.png"} width="100%" />
+                        </Box>
+                      </Grid>
+                    </Grid>
+                  </DialogContent>
+                </Dialog>
               </div>
             </div>
           </div>
--- src/pages/Landing/detailTwo.jsx
+++ src/pages/Landing/detailTwo.jsx
@@ -1,7 +1,15 @@
 import { ReactComponent as BuySvg } from "../../images/svg/buy.svg";
-import React from "react";
-import Popup from "reactjs-popup";
+import React, { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Box from '@mui/material/Box';
+
 function DetailTwo() {
+  const [open, setOpen] = useState(false)
   return (
     <div className="basic-3">
       <div className="second">
@@ -9,41 +17,37 @@ function DetailTwo() {
           <div className="row">
             <div className="col-lg-6">
               <div className="text-container">
-                <h2 style={{fontFamily: "Poppins"}}>Buy & Recieve</h2>
-                <p style={{fontFamily: "Poppins"}}>
+                <h2 style={{ fontFamily: "Poppins" }}>Buy & Recieve</h2>
+                <p style={{ fontFamily: "Poppins" }}>
                   As soon as you join our promotion sale you can confirm your
                   slot in the campaign by finishing your payment <br />
-                  After successfull campaign we will deliver your order right at
+                  After successful campaign we will deliver your order right at
                   your doorstep.
                 </p>
-                <Popup
-                  trigger={
-                    <button className="btn-solid-reg" style={{fontFamily: "Poppins"}}> Show More</button>
-                  }
-                  modal
-                >
-                  {(close) => (
-                    <div className="lightbox-basic">
-                      <div class="row">
-                        <button
-                          title="Close (Esc)"
-                          type="button"
-                          class="mfp-close x-button"
-                          onClick={close}
-                        >
-                          ×
-                        </button>
-                        <div class="col-lg-4">
-                          <img
-                            class="img-fluid"
-                            style={{height:"85%"}}
-                            src="images/checkout.png"
-                            alt="alternative"
-                          />
-                        </div>
-                        <div class="col-lg-4">
+                <button onClick={() => setOpen(true)} className="btn-solid-reg" style={{ fontFamily: "Poppins" }}> Show More</button>
+                <Dialog maxWidth={'md'} open={open} >
+                  <DialogTitle sx={{ m: 0, p: 2, backgroundColor: '#2f2c3d' }}>
+                    <IconButton
+                      onClick={() => setOpen(false)}
+                      sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                      }}
+                    >
+                      <CloseIcon />
+                    </IconButton>
+                  </DialogTitle>
+                  <DialogContent style={{ backgroundColor: '#2f2c3d', width: '100%' }}>
+                    <Grid container spacing={1}>
+                      <Grid item xs={12} sm={6} md={3} >
+                        <img src={"images/checkout.png"} width="100%" />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6} style={{ justifyContent: 'center' }}>
+                        <Box>
                           <h3>Buy & Recieve</h3>
-                          <hr />
+                          <Box style={{height: '3px', width: '20%', backgroundColor: '#ff9200', marginBottom: '4%'}}></Box>
                           <p>
                             With our premium subscription you can join as many
                             promotions as you want and enjoy faster home
@@ -54,7 +58,7 @@ function DetailTwo() {
                             the payment for the product without any hassle
                           </p>
                           <p>
-                            Once the campaign is successfull we will notify you
+                            Once the campaign is successful we will notify you
                             and deliver your product at your address
                           </p>
 
@@ -67,50 +71,47 @@ function DetailTwo() {
                           <table>
                             <tr>
                               <td class="icon-cell">
-                                <i class="fas fa-hand-holding-usd"></i>
+                                <i style={{ color: '#ff9200' }} class="fas fa-hand-holding-usd"></i>
                               </td>
-                              <td>Extra Discount on Your First Order</td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>Extra Discount on Your First Order</td>
                             </tr>
                             <tr>
                               <td class="icon-cell">
-                                <i class="far fa-credit-card"></i>
+                                <i style={{ color: '#ff9200' }} class="far fa-credit-card"></i>
                               </td>
-                              <td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>
                                 All Major Cards Accepted & EMI Options Available
                               </td>
                             </tr>
                             <tr>
                               <td class="icon-cell">
-                                <i class="fas fa-bell"></i>
+                                <i  style={{ color: '#ff9200' }}class="fas fa-bell"></i>
                               </td>
-                              <td>Notified After Campaign is Successfull</td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>Notified After Campaign is Successful</td>
                             </tr>
                             <tr>
                               <td class="icon-cell">
-                                <i class="fas fa-truck-moving"></i>
+                                <i style={{ color: '#ff9200' }} class="fas fa-truck-moving"></i>
                               </td>
-                              <td>Track Your Order</td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>Track Your Order</td>
                             </tr>
                             <tr>
                               <td class="icon-cell">
-                                <i class="fas fa-gift"></i>
+                                <i style={{ color: '#ff9200' }} class="fas fa-gift"></i>
                               </td>
-                              <td>Doorstep Delivery</td>
+                              <td style={{ paddingLeft: '5%', color: 'white' }}>Doorstep Delivery</td>
                             </tr>
                           </table>
-                        </div>
-                        <div class="col-lg-4">
-                          <img
-                            class="img-fluid"
-                            style={{height:"85%"}}
-                            src="images/banner.png"
-                            alt="alternative"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </Popup>
+                        </Box>
+                      </Grid>
+                      <Grid item xs={12} sm={3} md={3}>
+                        <Box sx={{ display: { xs:'none', sm: 'none', md: 'block'} }}>
+                          <img src={"images/banner.png"} width="100%" />
+                        </Box>
+                      </Grid>
+                    </Grid>
+                  </DialogContent>
+                </Dialog>
               </div>
             </div>
             <div className="col-lg-6">
